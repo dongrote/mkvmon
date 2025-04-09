3a6a86ad4e3a2485460e2abb783928f08188d31f
Don Grote <don.grote@gmail.com>
2025-04-09 09:33:59 -0400

    Switch to PollWatcher
    The recommended watcher was only producing events from the host
    for a CIFS mount; changed to PollWatcher.
    
    Bumped transcode parameters to slow down the transcoder since we only
    get notified when a file is created, not when it is closed by a writer.
    
    Handle SIGINT during the initial directory scan.
    Gracefully handle the situation when an enqueued file is removed prior
    to dequeue.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{env, path::{Path, PathBuf}, sync::{mpsc::{self, RecvTimeoutError, Send
 use std::fs;
 use filters::{PathFilter, PathExtensionFilter, NotFilter, VideoCodecFilter};
 use signal_hook::{consts::{SIGINT, SIGHUP, SIGTERM}, iterator::Signals};
-use notify::{self, event::{AccessKind, AccessMode}, EventKind, RecursiveMode, Watcher};
+use notify::{self, event::CreateKind, EventKind, RecursiveMode, Watcher};
 use now_str::now_string;
 use work_queue::WorkQueue;
 
@@ -30,7 +30,7 @@ fn main() {
     let (not_tx, not_rx) = mpsc::channel();
     let (wq_tx, wq_rx) = mpsc::channel();
     let mut wq = WorkQueue::new(Arc::clone(&stop), wq_rx);
-
+    let sig_stop = Arc::clone(&stop);
     thread::spawn(move || {
         if let Ok(mut signals) = Signals::new(&[SIGINT, SIGHUP, SIGTERM]) {
             println!("Listening for SIGINT, SIGHUP, SIGTERM");
@@ -43,7 +43,7 @@ fn main() {
                 };
 
                 {
-                    let mut s = stop.lock().unwrap();
+                    let mut s = sig_stop.lock().unwrap();
                     *s = true;
                 }
 
@@ -66,7 +66,7 @@ fn main() {
             Box::new(PathExtensionFilter::new("mkv")),
             Box::new(NotFilter::new(Box::new(VideoCodecFilter::new("hevc", "hvc1")))),
         ];
-        if let Ok(mut watcher) = notify::recommended_watcher(not_tx) {
+        if let Ok(mut watcher) = notify::PollWatcher::new(not_tx, notify::Config::default().with_poll_interval(Duration::from_secs(60))) {
             if let Ok(_) = watcher.watch(&watch_path, RecursiveMode::Recursive) {
                 loop {
                     let should_stop = {
@@ -82,22 +82,20 @@ fn main() {
                     match not_rx.recv_timeout(Duration::from_millis(100)) {
                         Ok(res) => match res {
                             Ok(event) => match event.kind {
-                                EventKind::Access(ak) => match ak {
-                                    AccessKind::Close(am) => match am {
-                                        AccessMode::Write => {
-                                            // wait until the file, opened for writing, has
-                                            // finished being written because we filter on file
-                                            // content and parsing a newly created, potentially
-                                            // empty file could cause us to fail a filter
-                                            dbg!(&event);
-                                            for p in &event.paths {
-                                                if filters.iter().all(|f| f.filter(Path::new(p))) {
-                                                    println!("{} enqueue {:?}", now_string(), &p);
-                                                    let _ = notify_wq_tx.send(p.clone());
+                                EventKind::Create(ck) => match ck {
+                                    CreateKind::Any => {
+                                        // wait until the file, opened for writing, has
+                                        // finished being written because we filter on file
+                                        // content and parsing a newly created, potentially
+                                        // empty file could cause us to fail a filter
+                                        for p in &event.paths {
+                                            if filters.iter().all(|f| f.filter(Path::new(p))) {
+                                                println!("{} enqueue {:?}", now_string(), &p);
+                                                if let Err(send_error) = notify_wq_tx.send(PathBuf::from(p)) {
+                                                    println!("{} notify_wq_tx.send() error: {:?}", now_string(), send_error);
                                                 }
                                             }
-                                        },
-                                        _ => (),
+                                        }
                                     },
                                     _ => (),
                                 },
@@ -121,19 +119,21 @@ fn main() {
     });
 
     // populate WorkQueue with initial scan
-    process_directory(&PathBuf::from(&args[1]), wq_tx.clone(), &filters);
+    process_directory(Arc::clone(&stop), &PathBuf::from(&args[1]), wq_tx.clone(), &filters);
 
     let _ = wq_handle.join();
     let _ = notify_handle.join();
 }
 
-fn process_directory(path: &PathBuf, tx: Sender<PathBuf>, filters: &Vec<Box<dyn PathFilter>>) {
+fn process_directory(stop: Arc<Mutex<bool>>, path: &PathBuf, tx: Sender<PathBuf>, filters: &Vec<Box<dyn PathFilter>>) {
     if let Ok(rd) = fs::read_dir(path) {
         let entries = rd.filter_map(|x| x.ok());
         for entry in entries {
+            let should_stop = { *stop.lock().unwrap() };
+            if should_stop { break; }
             if let Ok(metadata) = entry.metadata() {
                 if metadata.is_dir() {
-                    process_directory(&entry.path(), tx.clone(), filters);
+                    process_directory(Arc::clone(&stop), &entry.path(), tx.clone(), filters);
                 } else {
                     let p = entry.path();
                     if filters.iter().all(|f| f.filter(Path::new(&p))) {
